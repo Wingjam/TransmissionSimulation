efeda2150232505912c74e0f11b28416f1f2a2ad
jeepee12 <jeepee.12@hotmail.com>
2017-06-01 11:14:29 -0400

    Adding the main frame of the Frame

--- TransmissionSimulation/TransmissionSimulation/Models/Frame.cs
+++ TransmissionSimulation/TransmissionSimulation/Models/Frame.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections;
 
 namespace TransmissionSimulation.Models
 {
     class Frame
     {
+        private static ulong lastId = 0;
+
+        private ulong id;
+        private BitArray data;
+        private bool isAck;
+        private bool isNak;
+
+        /// <summary>
+        /// Constructor with the id auto generated. Used for transmitting information
+        /// </summary>
+        /// <param name="data"></param>
+        public Frame(BitArray data) : this(LastId(), data, false, false)
+        {
+        }
+
+        /// <summary>
+        /// Constructor for the response, giving the id of the frame.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="data"></param>
+        public Frame(ulong id, BitArray data, bool isAck) : this(id, data, isAck, !isAck)
+        {
+        }
+
+        /// <summary>
+        /// Constructor containg all the information.
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="data"></param>
+        /// <param name="isAck"></param>
+        /// <param name="isNak"></param>
+        private Frame(ulong id, BitArray data, bool isAck, bool isNak)
+        {
+            this.id = id;
+            this.data = data;
+            this.isAck = isAck;
+            this.isNak = isNak;
+        }
+
+        private static ulong LastId()
+        {
+            return lastId++;
+        }
     }
 }
