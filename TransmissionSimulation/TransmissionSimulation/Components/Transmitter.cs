dc9b558a51f66b046ad96a87805809b9013125c3
Raphaël Cayouette <raphael.cayouette@usherbrooke.ca>
2017-06-02 10:13:23 -0400

    Add data transfer
    -BitInversions
    -DataTransferMethod
    -ErrorInjection
    -Comments

--- TransmissionSimulation/TransmissionSimulation/Components/Transmitter.cs
+++ TransmissionSimulation/TransmissionSimulation/Components/Transmitter.cs
@@ -1,4 +1,4 @@
-﻿﻿﻿﻿﻿﻿﻿﻿using System;
+﻿using System;
 using System.Collections;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,56 +10,109 @@ namespace TransmissionSimulation.Components
     {
         private static Mutex mutex = new Mutex();
         private BitArray sendingSource;
-		private BitArray sendingDest;
+        private BitArray sendingDest;
         private BitArray receivingDest;
         private BitArray receivingSource;
         private bool readyToSendSource;
         private bool dataReceivedDest;
         private bool readyToSendDest;
         private bool dataReceivedSource;
-        private BitArray injectedError;
+        private uint bitInversions;
 
+        public uint BitInversions { set => bitInversions = value; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:TransmissionSimulation.Components.Transmitter"/> class.
+        /// </summary>
         public Transmitter()
         {
-            sendingSource = new BitArray(Constants.FrameSize*8);
-            sendingDest = new BitArray(Constants.FrameSize*8);
-			receivingSource = new BitArray(Constants.FrameSize * 8);
-			receivingDest = new BitArray(Constants.FrameSize * 8);
+            sendingSource = null;
+            sendingDest = null;
+            receivingSource = null;
+            receivingDest = null;
 
             Task.Factory.StartNew(() => CheckForTransferConditions(), TaskCreationOptions.LongRunning);
         }
 
+        /// <summary>
+        /// Checks for transfer conditions.
+        /// Loops in a separate thread.
+        /// </summary>
         private void CheckForTransferConditions()
         {
-            while(true)
+            while (true)
             {
+                mutex.WaitOne();
+
                 //Check for boolean conditions
+                if (!readyToSendSource && !dataReceivedDest)
+                {
+                    BitArray transferData = sendingSource;
+                    sendingSource = null;
+                    Thread.Sleep(Constants.DefaultDelay * 10); //deciseconds to milliseconds
+                    transferData = InjectError(transferData);
+                    receivingDest = transferData;
+                    readyToSendSource = true;
+                    dataReceivedDest = true;
+                }
+                else if (!readyToSendDest && !dataReceivedSource)
+                {
+                    BitArray transferData = sendingDest;
+                    sendingDest = null;
+                    Thread.Sleep(Constants.DefaultDelay*10);
+                    transferData = InjectError(transferData);
+                    receivingSource = transferData;
+                    readyToSendDest = true;
+                    dataReceivedSource = true;
+                }
+
+                mutex.ReleaseMutex();
             }
         }
 
-        private void TransferData()
+        /// <summary>
+        /// Injects errors randomly into the data to transfer based on the BitInversions property.
+        /// </summary>
+        /// <returns>Modified, partially erronous data.</returns>
+        /// <param name="transferData">Data to insert errors into.</param>
+        private BitArray InjectError(BitArray transferData)
         {
-			//http://www.c-sharpcorner.com/UploadFile/1d42da/threading-with-mutex/
-			//apply error if needed
-			//actually transfer data
-			//reset error
-		}
+            for (int i = 0; i < bitInversions; ++i)
+            {
+                int pos = new Random().Next(0, transferData.Length);
+                transferData[pos] = !transferData[pos];
+            }
 
-        public void InjectError(int bitInversions)
-        {
-            //make bit array based on random
+            bitInversions = 0;
+
+            return transferData;
         }
 
+        /// <summary>
+        /// Checks if the transmitter is ready to send data for the station.
+        /// </summary>
+        /// <returns><c>true</c>, if transmitter ready is ready for the station, <c>false</c> otherwise.</returns>
+        /// <param name="station">Station to check for transmitter ready.</param>
         public bool TransmitterReady(Constants.Station station)
         {
             return (station == Constants.Station.Source) ? readyToSendSource : readyToSendDest;
         }
 
-		public bool DataReceived(Constants.Station station)
-		{
-			return (station == Constants.Station.Source) ? dataReceivedSource : dataReceivedDest;
-		}
+        /// <summary>
+        /// Checks if data was received for the station.
+        /// </summary>
+        /// <returns><c>true</c>, if received was dataed, <c>false</c> otherwise.</returns>
+        /// <param name="station">Station that checks for data received.</param>
+        public bool DataReceived(Constants.Station station)
+        {
+            return (station == Constants.Station.Source) ? dataReceivedSource : dataReceivedDest;
+        }
 
+        /// <summary>
+        /// Sends the data from the station.
+        /// </summary>
+        /// <param name="data">Data to send.</param>
+        /// <param name="station">Station tto send data from.</param>
         public void SendData(BitArray data, Constants.Station station)
         {
             if (station == Constants.Station.Source)
@@ -67,8 +120,18 @@ namespace TransmissionSimulation.Components
                 sendingSource = data;
                 readyToSendSource = false;
             }
+            else
+            {
+                sendingDest = data;
+                readyToSendDest = false;
+            }
         }
 
+        /// <summary>
+        /// Gets the data available for the station.
+        /// </summary>
+        /// <returns>The data received.</returns>
+        /// <param name="station">Station that gets available data.</param>
         public BitArray GetData(Constants.Station station)
         {
             BitArray data = new BitArray(Constants.FrameSize * 8);
@@ -93,5 +156,5 @@ namespace TransmissionSimulation.Components
 
             return data;
         }
-	}
+    }
 }
