02c6844b7fb4e2271d97f287072607c489c2f656
Raphaël Cayouette <raphael.cayouette@usherbrooke.ca>
2017-06-06 18:42:58 -0400

    Add unit tests for Transmitter

---
+++ TransmissionSimulation/TransmissionSimulationTests/Helpers/TransmitterTests.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransmissionSimulation.Helpers;
+using TransmissionSimulation.Components;
+using TransmissionSimulation.Ressources;
+using System.Threading;
+
+namespace TransmissionSimulationTests.Helpers
+{
+    [TestClass]
+    public class TransmitterTests
+    {
+        Transmitter t;
+        BitArray data;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            t = new Transmitter();
+            data = new BitArray(7);
+        }
+
+        [TestMethod]
+        public void When_data_is_sent_then_it_is_received()
+        {
+            bool before = t.DataReceivedDest;
+            Assert.IsTrue(t.TransmitterReady(Constants.Station.Source));
+            t.SendData(data, Constants.Station.Source);
+            Assert.AreNotEqual(before, t.DataReceivedDest);
+        }
+
+        [TestMethod]
+        public void When_data_is_sent_but_not_obtained_then_transmitter_is_not_ready()
+        {
+            Assert.IsTrue(t.TransmitterReady(Constants.Station.Source));
+            t.SendData(data, Constants.Station.Source);
+            Assert.IsFalse(t.TransmitterReady(Constants.Station.Source));
+        }
+
+        [TestMethod]
+        public void When_data_is_sent_and_obtained_then_transmitter_is_ready()
+        {
+            Assert.IsTrue(t.TransmitterReady(Constants.Station.Source));
+            t.SendData(data, Constants.Station.Source);
+            var dataDest = t.GetData(Constants.Station.Dest);
+            Assert.IsTrue(t.TransmitterReady(Constants.Station.Source));
+        }
+    }
+}
